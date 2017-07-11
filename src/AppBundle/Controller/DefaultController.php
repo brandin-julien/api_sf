b630a0375a6777f14425e2fa327d5866ea6e6dc5
sylvainSUPINTERNET <sylvain.joly00@orange.fr>
2017-07-11 18:20:32 +0200

    delete with id message error

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -9,6 +9,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class DefaultController extends Controller
 {
@@ -107,6 +108,11 @@ class DefaultController extends Controller
      */
     public function deleteTaskAction(Request $request, EntityManager $em, $id)
     {
+
+        if($id == null){
+            throw $this->createNotFoundException('Unable to task to delete');
+        }
+
         $taskToDelete = $em->getRepository('AppBundle:todoList')->find($id);
 
         if($taskToDelete != null){
